69bedbd79513f1cbbcc9b08d7c89fef36609621c
Pablo Porto <pablosebastianporto@gmail.com>
2023-02-28 17:42:33 -0300

    tercer escritura en el repo

--- .history/CVFINAL/CVfinal_20230228140612.js
+++
@@ -1,9 +0,0 @@
-// Seleccionar todos los elementos <a> dentro del elemento <nav> y añadir un event listener
-document.querySelectorAll("header nav a").forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault(); // prevenir el comportamiento predeterminado del enlace
-    const targetId = event.target.getAttribute("href"); // obtener el ID del elemento de destino
-    const targetElement = document.querySelector(targetId); // seleccionar el elemento de destino
-    targetElement.scrollIntoView({ behavior: "smooth" }); // desplazarse hacia el elemento de destino con desplazamiento suave
-  });
-});
--- .history/CVFINAL/CVfinal_20230228154453.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelectorAll(".nav a");
-navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const sectionId = e.target.getAttribute("href");
-    const section = document.querySelector(sectionId);
-    section.scrollIntoView({ behavior: "smooth" });
-  });
-});
